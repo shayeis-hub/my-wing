a91d8f0e170944db31f508bac7f7949017345636
Shay <shayeis@gmail.com>
2026-06-06 00:59:36 +0300

    Enable pinch-to-zoom in the Android WebView (disabled by default in Capacitor)

--- android/app/src/main/java/app/wingpact/android/MainActivity.java
+++ android/app/src/main/java/app/wingpact/android/MainActivity.java
@@ -1,5 +1,18 @@
 package app.wingpact.android;
 
+import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Enable pinch-to-zoom in the WebView (disabled by default in Capacitor).
+        if (bridge != null && bridge.getWebView() != null) {
+            WebSettings settings = bridge.getWebView().getSettings();
+            settings.setSupportZoom(true);
+            settings.setBuiltInZoomControls(true);
+            settings.setDisplayZoomControls(false);
+        }
+    }
+}
